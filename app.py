d08fb7fe3c0ea31de960afc510d65c1bb49a02bc
DiarmaiddeBúrca <diarmaid.deburca@everycs.com>
2020-03-01 21:19:45 +0100

    Various Updates, setting default not found page

--- app.py
+++ app.py
@@ -1,22 +1,45 @@
 # Originally from https://www.starlette.io/templates/
+from starlette.responses import HTMLResponse
 from starlette.applications import Starlette
 from starlette.routing import Route, Mount
 from starlette.staticfiles import StaticFiles
-from application.pages import index_page, about_me, fortran_tutorial_one, input_fortran_code, show_fortran_results
+from application.pages import (
+    index_page,
+    about_me,
+    fortran_tutorial_one,
+    input_fortran_code,
+    show_fortran_results,
+)
+from application.error_pages import (
+    four_oh_four_not_found,
+    server_error,
+    four_oh_five_not_found,
+)
 
 
 def start_up():
-	pass
+    pass
+
+
+exception_handlers = {
+    404: four_oh_four_not_found,
+    405: four_oh_five_not_found,
+    500: server_error,
+}
+
 
 routes = [
     Route("/index", endpoint=index_page),
-    Route("/about_me", endpoint = about_me),
-    #Route("/fortran1", endpoint = fortran_tutorial_one),
-	Route("/runfortran", endpoint = input_fortran_code, methods = ["GET", "POST"]),
-	Route("/show_fortran_results", endpoint = show_fortran_results, methods = ["POST"]),
-	Mount("/static", StaticFiles(directory="./static"), name="static"),
-
+    Route("/about_me", endpoint=about_me),
+    # Route("/fortran1", endpoint = fortran_tutorial_one),
+    Route("/runfortran", endpoint=input_fortran_code, methods=["GET", "POST"]),
+    Route("/show_fortran_results", endpoint=show_fortran_results, methods=["POST"]),
+    Mount("/static", StaticFiles(directory="./static"), name="static"),
 ]
 
-app = Starlette(routes=routes, debug=True, on_startup = [start_up])
-
+app = Starlette(
+    routes=routes,
+    debug=True,
+    on_startup=[start_up],
+    exception_handlers=exception_handlers,
+)
---
+++ application/error_pages.py
@@ -0,0 +1,22 @@
+from starlette.responses import HTMLResponse
+from application import templates
+
+HTML_404_PAGE = """$=$ NOT FOUND"""
+HTML_405_PAGE = """405 Not Found"""
+HTML_500_PAGE = """500 Page not found"""
+
+
+async def not_found(request, exc):
+    return templates.TemplateResponse("404_not_found.html", {"request": request, "code": exc})
+
+async def server_error(request, exc):
+    return HTMLResponse(content=HTML_500_PAGE, status_code=exc.status_code)
+
+
+async def four_oh_four_not_found(request, exc):
+    return templates.TemplateResponse("404_not_found.html", {"request": request, "code": exc})
+
+
+async def four_oh_five_not_found(request, exc):
+    return HTMLResponse(content=HTML_404_PAGE, status_code=exc.status_code)
+
